9e48a96c5e2c04b2c651c4b35ce4fdee99ea88e8
kanegon <kanegon9365@gmail.com>
2019-03-24 09:19:36 +0900

    ソート機能追加

--- app/controllers/hitter18s_controller.rb
+++ app/controllers/hitter18s_controller.rb
@@ -1,10 +1,21 @@
 class Hitter18sController < ApplicationController
+helper_method :sort_column, :sort_direction
+
   def index
-    @hitter18s = Hitter18.all
+    @hitter18s = Hitter18.all.order(sort_column + ' ' + sort_direction)
   end
 
   def show
     @hitter18 = Hitter18.find(params[:id])
   end
   
+  private
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    end
+
+    def sort_column
+      Hitter18.column_names.include?(params[:sort]) ? params[:sort] : "id"
+    end
 end
